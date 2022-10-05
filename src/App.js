31eb3005d60ea4e941c028490580946f07d2dafa
HannaVelychko <anna.ua19@gmail.com>
2022-10-05 23:04:07 +0200

    Added Search engine

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Hanna Velychko and is open-sourced on
+            GitHub and hosted on Netlify
+          </small>
+        </footer>
+      </div>
+    </div>
+  );
+}
