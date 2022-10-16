2cfc6b48739110b5833e37dafd9f581aebec16d2
HannaVelychko <anna.ua19@gmail.com>
2022-10-17 00:02:29 +0200

    Added defaultKeyword

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,44 +3,57 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState(null);
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
-
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <label>What word do you want to look up?</label>
-          <br />
-          <input
-            type="search"
-            placeholder="Search for a word"
-            autoFocus={true}
-            onChange={handleKeywordChange}
-            className="form-control search-input"
-          />
-        </form>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label>What word do you want to look up?</label>
+            <br />
+            <input
+              type="search"
+              placeholder="Search for a word"
+              autoFocus={true}
+              onChange={handleKeywordChange}
+              className="form-control search-input"
+            />
+          </form>
+          <small className="hint">i.e. sunrise, coding, yoga</small>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
